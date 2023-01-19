4cdadbc1d3f4f6fc51f62a1041e19535a85e7b24
Saam Wesley <65556254+Saam09@users.noreply.github.com>
2023-01-19 11:57:00 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -2,8 +2,7 @@ class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                    if i != j:
-                        x = nums[i] + nums[j]
-                        if x == target:
-                            a = [i,j]
-                            return a
+                    x = nums[i] + nums[j]
+                    if x == target:
+                        a = [i,j]
+                        return a
